bd8aa43139f01b6ea5a191709f19dc44c348efa5
Danilo <Danilo@192.168.0.87>
2019-04-12 10:52:50 -0300

    OauthServer first commit

---
+++ OauthServer/src/com/oauth/demo/ServerContent.java
@@ -0,0 +1,10 @@
+package com.oauth.demo;
+
+public class ServerContent {
+    public static final String CLIENT_ID = "098045377";
+    public static final String CLIENT_SECRET = "1234";
+    public static final String ISSUED_AT = "0123456789";
+    public static final Long EXPIRES_IN = 987654321l;
+    
+    public static final String ACCESS_TOKEN = "access_token_valid";
+}
